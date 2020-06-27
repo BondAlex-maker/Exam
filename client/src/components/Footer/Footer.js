8ec2dc3ecf91456d6488db79f32d391e16d693de
bondalex-maker <bondarenko.alex.sergeevich@gmail.com>
2020-06-27 16:00:46 +0300

    Reformat and create new elements in Footer and Header

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,27 +4,72 @@ import CONSTANTS from '../../constants';
 
 
 class Footer extends Component {
+    AnyItemsRender = (item) => {
+        if (item.title){
+            return (
+                <div>
+                    <h4>{item.title}</h4>
+                    {item.items.map(i => <a key={i} href="https://google.com">{i}</a>)}
+                </div>
+            )
+        }else {
+            return (
+                <div>
+                    {item.items.map(i => <a key={i} href="https://google.com">{i}</a>)}
+                </div>
+            );
+        }
 
-    topFooterItemsRender = (item) => {
-        return (
-            <div key={item.title}>
-                <h4>{item.title}</h4>
-                {item.items.map(i => <a key={i} href="https://google.com">{i}</a>)}
-            </div>
-        );
     };
 
-    topFooterRender() {
-        return CONSTANTS.FooterItems.map(item => this.topFooterItemsRender(item))
+    FooterItemsRender() {
+        return CONSTANTS.FooterItems.map(item => this.AnyItemsRender(item))
     };
+
+    FooterBottomItemsRender() {
+        return CONSTANTS.FooterBottomItems.map(item => this.AnyItemsRender(item))
+    };
+
+
 
     render() {
         return (
             <div className={styles.footerContainer}>
                 <div className={styles.footerTop}>
                     <div>
-                        {this.topFooterRender()}
+                        {this.FooterItemsRender()}
                     </div>
+                    <h1>FEATURED CATEGORIES</h1>
+                    <div>
+                        {this.FooterBottomItemsRender()}
+                    </div>
+
+                </div>
+                <div className={styles.footerBottom}>
+                    <div className={styles.container}>
+                        <div className={styles.itemsRow}>
+                            <div>
+                                <a href="http://google.com" >
+                                    <img src="https://howbrandsarebuilt.com/wp-content/themes/howbab/images/squadhelp.png" alt="SquadHelp" className={styles.size}/>
+                                </a>
+                                <a href="http://google.com">
+                                    Squadhelp.com has a Shopper Approved rating of <br/>
+                                    4.9/5 based on 2684 ratings and reviews
+                                </a>
+                            </div>
+                            <div className={styles.flex}>
+                                <p>Copyright © 2020 Squadhelp Inc</p>
+                                <ul>
+                                    <li><a href="http://google.com"><i className="fa fa-facebook" aria-hidden="true"/></a></li>
+                                    <li><a href="http://google.com"><i className="fa fa-twitter" aria-hidden="true"/></a></li>
+                                </ul>
+                            </div>
+                            <div>
+                                
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         )
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import CONSTANTS from '../../constants';
 import {getUserAction, clearUserStore, headerRequest} from '../../actions/actionCreator';
+import Toggle from "../Menu";
+import Menu from "../Menu";
 
 
 class Header extends React.Component{
@@ -57,12 +59,14 @@ class Header extends React.Component{
         }
     };
 
+
     render() {
+
         if (this.props.isFetching) {
             return null;
         }
         return (
-            <div className={styles.headerContainer}>
+            <div className={styles.headerContainer} >
                 <div className={styles.fixedHeader}>
                     <span className={styles.info}>Squadhelp recognized as one of the Most Innovative Companies by Inc Magazine.</span>
                     <a href="http://www.google.com">Read Announcement</a>
@@ -76,82 +80,10 @@ class Header extends React.Component{
                         {this.renderLoginButtons()}
                     </div>
                 </div>
-                <div className={styles.navContainer}>
-                    <img src={`${CONSTANTS.STATIC_IMAGES_PATH}blue-logo.png`} className={styles.logo} alt='blue_logo'/>
-                    <div className={styles.leftNav}>
-                        <div className={styles.nav}>
-                            <ul>
-                                <li>
-                                    <span>NAME IDEAS</span><img src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                                                                alt='menu'/>
-                                    <ul>
-                                        <li><a href="http://www.google.com">Beauty</a></li>
-                                        <li><a href="http://www.google.com">Consulting</a></li>
-                                        <li><a href="http://www.google.com">E-Commerce</a></li>
-                                        <li><a href="http://www.google.com">Fashion & Clothing</a></li>
-                                        <li><a href="http://www.google.com">Finance</a></li>
-                                        <li><a href="http://www.google.com">Real Estate</a></li>
-                                        <li><a href="http://www.google.com">Tech</a></li>
-                                        <li className={styles.last}><a href="http://www.google.com">More Categories</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <span>CONTESTS</span><img src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                                                              alt='menu'/>
-                                    <ul>
-                                        <li><a href="http://www.google.com">HOW IT WORKS</a></li>
-                                        <li><a href="http://www.google.com">PRICING</a></li>
-                                        <li><a href="http://www.google.com">AGENCY SERVICE</a></li>
-                                        <li><a href="http://www.google.com">ACTIVE CONTESTS</a></li>
-                                        <li><a href="http://www.google.com">WINNERS</a></li>
-                                        <li><a href="http://www.google.com">LEADERBOARD</a></li>
-                                        <li className={styles.last}><a href="http://www.google.com">BECOME A
-                                            CREATIVE</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <span>Our Work</span><img src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                                                              alt='menu'/>
-                                    <ul>
-                                        <li><a href="http://www.google.com">NAMES</a></li>
-                                        <li><a href="http://www.google.com">TAGLINES</a></li>
-                                        <li><a href="http://www.google.com">LOGOS</a></li>
-                                        <li className={styles.last}><a href="http://www.google.com">TESTIMONIALS</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <span>Names For Sale</span>
-                                    <img src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`} alt='menu'/>
-                                    <ul>
-                                        <li><a href="http://www.google.com">POPULAR NAMES</a></li>
-                                        <li><a href="http://www.google.com">SHORT NAMES</a></li>
-                                        <li><a href="http://www.google.com">INTRIGUING NAMES</a></li>
-                                        <li><a href="http://www.google.com">NAMES BY CATEGORY</a></li>
-                                        <li><a href="http://www.google.com">VISUAL NAME SEARCH</a></li>
-                                        <li className={styles.last}><a href="http://www.google.com">SELL YOUR
-                                            DOMAINS</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <span>Blog</span><img src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                                                          alt='menu'/>
-                                    <ul>
-                                        <li><a href="http://www.google.com">ULTIMATE NAMING GUIDE</a></li>
-                                        <li><a href="http://www.google.com">POETIC DEVICES IN BUSINESS NAMING</a></li>
-                                        <li><a href="http://www.google.com">CROWDED BAR THEORY</a></li>
-                                        <li className={styles.last}><a href="http://www.google.com">ALL ARTICLES</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                        {this.props.data && this.props.data.role !== CONSTANTS.CREATOR &&
-                        <div className={styles.startContestBtn} onClick={this.startContests}>START CONTEST</div>}
-                    </div>
-                </div>
+                    <Menu></Menu>
+
             </div>
+
         )
     }
 }
