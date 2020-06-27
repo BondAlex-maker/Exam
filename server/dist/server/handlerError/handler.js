2a93bcf39953a7f53c91b60bcc23349ff6e94d92
bondalex-maker <bondarenko.alex.sergeevich@gmail.com>
2020-06-27 16:15:27 +0300

    etc

---
+++ server/dist/server/handlerError/handler.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var logger = require('../utils/logger');
+
+module.exports = function (err, req, res, next) {
+  logger.log(err);
+  console.log(err);
+
+  if (err.message === 'new row for relation "Banks" violates check constraint "Banks_balance_ck"' || err.message === 'new row for relation "Users" violates check constraint "Users_balance_ck"') {
+    err.message = 'Not Enough money';
+    err.code = 406;
+  }
+
+  if (!err.message || !err.code) {
+    res.status(500).send('Server Error');
+  } else {
+    res.status(err.code).send(err.message);
+  }
+};
